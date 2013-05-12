aff787aec1f4c7b6955a53d968167c87fcd994f8
Laurent <Foxp2projects@gmail.com>
2013-05-12 11:15:20 +0200

    added : language : french : modules : action recorder

---
+++ includes/languages/french/modules/action_recorder/ar_reset_password.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ACTION_RECORDER_RESET_PASSWORD_TITLE', 'Modification du mot de passe des clients');
+  define('MODULE_ACTION_RECORDER_RESET_PASSWORD_DESCRIPTION', 'Enregistre l\'utilisation du module lors des modifications des mots de passe par les clients');
+?>
