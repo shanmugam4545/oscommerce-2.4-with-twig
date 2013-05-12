e5f6ea7246a9162ea2105620cac186cefcfea7c5
Laurent <Foxp2projects@gmail.com>
2013-05-12 10:03:51 +0200

    added : language : french : module social bookmarks

---
+++ includes/languages/french/modules/social_bookmarks/sb_email.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_SOCIAL_BOOKMARKS_EMAIL_TITLE', 'Mail');
+  define('MODULE_SOCIAL_BOOKMARKS_EMAIL_DESCRIPTION', 'Permettre de faire connaitre les produits (partager) via Mail.');
+  define('MODULE_SOCIAL_BOOKMARKS_EMAIL_PUBLIC_TITLE', 'Partager par Mail');
+?>
---
+++ includes/languages/french/modules/social_bookmarks/sb_twitter_button.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_SOCIAL_BOOKMARKS_TWITTER_BUTTON_TITLE', 'Bouton Twitter');
+  define('MODULE_SOCIAL_BOOKMARKS_TWITTER_BUTTON_DESCRIPTION', 'Permettre de faire connaitre les produits (partager) avec le bouton Twitter.');
+  define('MODULE_SOCIAL_BOOKMARKS_TWITTER_BUTTON_PUBLIC_TITLE', 'Tweet');
+?>
