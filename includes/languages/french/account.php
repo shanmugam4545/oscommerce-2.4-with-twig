cc83b401f26fc1bc6d394351e3999fdc268fc699
Laurent <Foxp2projects@gmail.com>
2013-05-12 20:48:48 +0200

    added : language : french : file : account

---
+++ includes/languages/french/account.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ *
+ * @copyright Copyright (c) 2013 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
+
+define('NAVBAR_TITLE', 'Mon compte');
+define('NAVBAR_TITLE_EDIT', 'Modifier compte');
+define('NAVBAR_TITLE_ADDRESS_BOOK', 'Carnet d\'adresses');
+define('NAVBAR_TITLE_ADDRESS_BOOK_NEW', 'Nouvelle entrée');
+define('NAVBAR_TITLE_ADDRESS_BOOK_EDIT', 'Mise à jour entrée');
+define('NAVBAR_TITLE_ADDRESS_BOOK_NEW_DELETE', 'Supprimer entrée');
+define('NAVBAR_TITLE_PASSWORD', 'Changer le mot de passe');
+define('NAVBAR_TITLE_ORDERS', 'Historique');
+define('NAVBAR_TITLE_ORDERS_INFO', 'Commande #%s');
+define('NAVBAR_TITLE_NEWSLETTERS', 'Abonnement aux bulletins d\'information');
+define('NAVBAR_TITLE_NOTIFICATIONS', 'Alertes produits');
+define('NAVBAR_TITLE_LOGIN', 'Ouverture de session');
+define('NAVBAR_TITLE_CREATE', 'Créer un compte');
+define('NAVBAR_TITLE_CREATE_SUCCESS', 'Succès');
+define('NAVBAR_TITLE_LOGOFF', 'Fermer la session');
+define('NAVBAR_TITLE_PASSWORD_FORGOTTEN', 'Mot de passe oublié');
+define('NAVBAR_TITLE_PASSWORD_RESET', 'Mise à jour du mot de passe');
+
+define('HEADING_TITLE', 'Détails de mon compte');
+define('HEADING_TITLE_EDIT', 'Information de mon compte');
+define('HEADING_TITLE_ADDRESS_BOOK', 'Carnet d\'adresses personnel');
+define('HEADING_TITLE_ADDRESS_BOOK_NEW', 'Nouvelle entrée carnet d\'adresses');
+define('HEADING_TITLE_ADDRESS_BOOK_EDIT', 'Mise à jour entrée carnet d\'adresses');
+define('HEADING_TITLE_ADDRESS_BOOK_DELETE', 'Supprimer entrée carnet d\'adresses');
+define('HEADING_TITLE_PASSWORD', 'Mon mot de passe');
+define('HEADING_TITLE_ORDERS', 'Historique de mes commandes');
+define('HEADING_TITLE_ORDERS_INFO', 'Information commande');
+define('HEADING_TITLE_NEWSLETTERS', 'Abonnement aux bulletins d\'information');
+define('HEADING_TITLE_NOTIFICATIONS', 'Alertes produits');
+define('HEADING_TITLE_LOGIN', 'Bienvenue, veuillez ouvrir une session');
+define('HEADING_TITLE_CREATE', 'Information de mon compte');
+define('HEADING_TITLE_CREATE_SUCCESS', 'Votre compte a été créé !');
+define('HEADING_TITLE_LOGOFF', 'Fermer la session');
+define('HEADING_TITLE_PASSWORD_FORGOTTEN', 'J\'ai oublié mon mot de passe !');
+define('HEADING_TITLE_PASSWORD_RESET', 'Réinitialisation du mot de passe');
+
+define('OVERVIEW_TITLE', 'Aperçu général');
+define('OVERVIEW_SHOW_ALL_ORDERS', '(Afficher toutes mes commandes)');
+define('OVERVIEW_PREVIOUS_ORDERS', 'Commandes précédentes');
+
+define('MY_ACCOUNT_TITLE', 'Mon compte');
+define('MY_ACCOUNT_INFORMATION', 'Afficher ou modifier les informations de mon compte.');
+define('MY_ACCOUNT_ADDRESS_BOOK', 'Afficher ou modifier des entrées dans mon carnet d\'adresses.');
+define('MY_ACCOUNT_PASSWORD', 'Modifier le mot de passe de mon compte.');
+
+define('MY_ORDERS_TITLE', 'Mes commandes');
+define('MY_ORDERS_VIEW', 'Afficher les commandes que j\'ai effectuées.');
+
+define('EMAIL_NOTIFICATIONS_TITLE', 'Alertes par courrier électronique');
+define('EMAIL_NOTIFICATIONS_NEWSLETTERS', 'S\'abonner ou se désabonner aux bulletins d\'information.');
+define('EMAIL_NOTIFICATIONS_PRODUCTS', 'Afficher ou modifier ma liste d\'alertes produit.');
+
+define('SUCCESS_ACCOUNT_UPDATED', 'Votre compte a été mis à jour.');
+
+define('PRIMARY_ADDRESS_TITLE', 'Adresse principale');
+define('PRIMARY_ADDRESS_DESCRIPTION', 'Cette adresse est employée comme adresse principale d\'expédition et de facturation pour des commandes passées sur ce magasin.<br /><br />Cette adresse est également employée comme base pour le produit et les calculs d\'imp&ocirc;ts sur le service.');
+
+define('ADDRESS_BOOK_TITLE', 'Entrées du carnet d\'adresses');
+
+define('PRIMARY_ADDRESS', '(Adresse principale)');
+
+define('TEXT_MAXIMUM_ENTRIES', '<strong>REMARQUE :</strong> Un maximum de %s entrées dans le carnet d\'adresses est autorisé.');
+
+define('HEADING_TITLE_ADD_ENTRY', 'Nouvelle entrée carnet d\'adresses');
+define('HEADING_TITLE_MODIFY_ENTRY', 'Mise à jour de l\'entrée dans votre carnet d\'adresse');
+
+define('DELETE_ADDRESS_TITLE', 'Supprimer adresse');
+define('DELETE_ADDRESS_DESCRIPTION', 'Etes-vous sûr de vouloir supprimer l\'adresse sélectionnée dans votre carnet d\'adresses ?');
+
+define('ADDRESS_BOOK_ENTRY', 'Entrée du carnet d\'adresse');
+
+define('SELECTED_ADDRESS', 'Adresse choisie');
+define('SET_AS_PRIMARY', 'Sélectionner comme adresse principale.');
+
+define('SUCCESS_ADDRESS_BOOK_ENTRY_DELETED', 'L\'adresse sélectionner a été supprimée de votre carnet d\'adresses.');
+define('SUCCESS_ADDRESS_BOOK_ENTRY_UPDATED', 'Votre carnet d\'adresses a été mis à jour.');
+
+define('WARNING_PRIMARY_ADDRESS_DELETION', 'L\'adresse principale ne peut pas être suprimée. Veuillez sélectionner une autre adresse comme adresse principale et réessayez.');
+
+define('ERROR_NONEXISTING_ADDRESS_BOOK_ENTRY', 'L\'entrée du carnet d\'adresses n\'existe pas. ');
+define('ERROR_ADDRESS_BOOK_FULL', 'Votre carnet d\'adresses est plein. Veuillez supprimer une adresse inutile pour en enregistrer une nouvelle.');
+
+define('MY_PASSWORD_TITLE', 'Mon mot de passe');
+
+define('SUCCESS_PASSWORD_UPDATED', 'Votre mot de passe a été modifié.');
+define('ERROR_CURRENT_PASSWORD_NOT_MATCHING', 'Votre nouveau mot de passe doit être différent du mot de passe actuellement enregistré sur notre site. Veuillez réessayer.');
+
+define('TEXT_ORDER_NUMBER', 'Numéro de commande :');
+define('TEXT_ORDER_STATUS', 'Statut de la commande :');
+define('TEXT_ORDER_DATE', 'Date de la commande :');
+define('TEXT_ORDER_SHIPPED_TO', 'Expédiée à :');
+define('TEXT_ORDER_BILLED_TO', 'Facturé à :');
+define('TEXT_ORDER_PRODUCTS', 'Produits :');
+define('TEXT_ORDER_COST', 'Coût de la commande :');
+define('TEXT_VIEW_ORDER', 'Afficher commande');
+
+define('TEXT_NO_PURCHASES', 'Vous n\'avez pas encore fait d\'achat.');
+
+define('HEADING_ORDER_NUMBER', 'Commande #%s');
+define('HEADING_ORDER_DATE', 'Date de la commande :');
+define('HEADING_ORDER_TOTAL', 'Total de la commande :');
+
+define('HEADING_DELIVERY_ADDRESS', 'Adresse de livraison');
+define('HEADING_SHIPPING_METHOD', 'Mode de livraison');
+
+define('HEADING_PRODUCTS', 'Produits');
+define('HEADING_TAX', 'Taxe');
+define('HEADING_TOTAL', 'Total');
+
+define('HEADING_BILLING_INFORMATION', 'Information facturation');
+define('HEADING_BILLING_ADDRESS', 'Adresse de facturation');
+define('HEADING_PAYMENT_METHOD', 'Mode de paiement');
+
+define('HEADING_ORDER_HISTORY', 'Historique de la commande');
+define('HEADING_COMMENT', 'Commentaires');
+define('TEXT_NO_COMMENTS_AVAILABLE', 'Pas de commentaire.');
+
+define('TABLE_HEADING_DOWNLOAD_DATE', 'Liens expirés : ');
+define('TABLE_HEADING_DOWNLOAD_COUNT', ' Téléchargements restants');
+define('HEADING_DOWNLOAD', 'Liens de téléchargement');
+
+define('MY_NEWSLETTERS_TITLE', 'Mes abonnements aux bulletins d\'informations');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER', 'Bulletin d\'informations général');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER_DESCRIPTION', 'Concerne les actualités du site, les nouveaux produits, les offres spéciales, et autres annonces promotionnelles.');
+
+define('SUCCESS_NEWSLETTER_UPDATED', 'Vos abonnements au bulletin d\'informations ont été mis à jour.');
+
+define('MY_NOTIFICATIONS_TITLE', 'Mes alertes produits');
+define('MY_NOTIFICATIONS_DESCRIPTION', 'La liste des alertes produit vous permet de rester informé sur les produits qui vous intéressent.<br /><br />Pour être informé de tout changement sur les produits, choisissez <strong>alerte globale</strong>.');
+
+define('GLOBAL_NOTIFICATIONS_TITLE', 'Alerte globale');
+define('GLOBAL_NOTIFICATIONS_DESCRIPTION', 'Recevoir des alertes sur tous les produits du catalogue.');
+
+define('NOTIFICATIONS_TITLE', 'Alertes produits');
+define('NOTIFICATIONS_DESCRIPTION', 'Pour supprimer une alerte de produit, décochez la case en face du produit et cliquez sur Continuer.');
+define('NOTIFICATIONS_NON_EXISTING', 'Il n\'y a actuellement aucun produit coché.<br /><br />Pour ajouter des produits à votre liste de d\'alertes, cliquez sur le lien alerte disponible sur la page produit.');
+
+define('SUCCESS_NOTIFICATIONS_UPDATED', 'Vos alertes produit ont été mises à jour.');
+
+define('HEADING_NEW_CUSTOMER', 'Nouveau client');
+define('TEXT_NEW_CUSTOMER', 'Je suis un nouveau client.');
+define('TEXT_NEW_CUSTOMER_INTRODUCTION', 'En créant votre compte sur ' . STORE_NAME . ' vous pourrez faire vos achats plus rapidement, garder votre panier d\'une visite à l\'autre et suivre vos commandes.');
+
+define('HEADING_RETURNING_CUSTOMER', 'Client enregistré');
+define('TEXT_RETURNING_CUSTOMER', 'J\'ai déjà commandé.');
+
+define('TEXT_PASSWORD_FORGOTTEN', 'Vous avez oublié votre mot de passe ? Cliquez ici.');
+
+define('TEXT_LOGIN_ERROR', 'Erreur : aucun résultat à cette adresse électronique et/ou mot de passe.');
+define('TEXT_VISITORS_CART', '<strong>REMARQUE :</strong> Le contenu de votre &quot;panier visiteurs&quot; sera ajouté à celui de votre &quot;panier membres&quot; dès que vous aurez ouvert une session. <a href="javascript:session_win();">[Plus d\'info]</a>');
+
+define('TEXT_ORIGIN_LOGIN', '<small><strong>REMARQUE:</strong></small> Si vous avez déjà un compte chez nous, veuillez vous connecter à la page d\'<a href="%s"><u>ouverture de session</u></a>.');
+
+define('EMAIL_SUBJECT', 'Bienvenue sur ' . STORE_NAME);
+define('EMAIL_GREET_MR', 'Cher Mr. %s,' . "\n\n");
+define('EMAIL_GREET_MS', 'Cher Mme. %s,' . "\n\n");
+define('EMAIL_GREET_NONE', 'Cher %s' . "\n\n");
+define('EMAIL_WELCOME', 'Nous sommes heureux de vous accueillir dans notre boutique en ligne <strong>' . STORE_NAME . '</strong>.' . "\n\n");
+define('EMAIL_TEXT', 'Vous pouvez maintenant utiliser les <strong>services réservés</strong> à nos clients :'. "\n" . '<li><strong>Panier permanent</strong></li> - Tous les produits ajoutés à votre panier en ligne sont conservés entre vos visites jusqu\'à la finalisation de votre commande. Vous pouvez donc commencer vos achats aujourd\'hui et conclure votre commande lors de votre prochaine visite.'. "\n" . '<li><strong>Carnet d\'adresses </strong></li> - Nous pouvons livrer vos produits à une adresse différente de la vôtre ! C\'est parfait pour envoyer des cadeaux à vos proches et amis.'. "\n" . '<li><strong>Historique des commandes</strong></li> - Chacune de vos commandes dans notre boutique est conservée. Vous pouvez à tout moment consulter l\'historique ou connaître l\'avancement de votre dernière commande si elle n\'a pas encore été livrée.'. "\n\n");
+define('EMAIL_CONTACT', 'Pour plus d\'informations à propos de nos services en ligne, vous pouvez contacter le gestionnaire de la boutique par courrier électronique : ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n");
+define('EMAIL_WARNING', '<strong><u>REMARQUE IMPORTANTE :</u></strong>Vous recevez cet email car il fait suite à l\'inscription d\'un nouveau client dans notre boutique en ligne. Si vous ne vous êtes pas inscrit sur ' . STORE_NAME . ', merci de le signaler au gestionnaire de la boutique à cette adresse : ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n");
+
+define('SUCCESS_TEXT_ACCOUNT_CREATED', 'Félicitations ! Votre nouveau compte a été créé ! Vous pouvez maintenant profiter des privilèges de membre afin de tirer pleinement parti de notre site de commerce en ligne. Pour <small><strong>TOUTE</strong></small> question sur le fonctionnement de ce site, veuillez envoyer un courrier électronique au <a href="' . osc_href_link('info','contact') . '">gestionnaire du site</a>.<br /><br />Une confirmation de création de compte a été envoyée à l\'adresse électronique fournie. Si vous ne l\'avez pas reçu dans l\'heure, veuillez  <a href="' . osc_href_link('info','contact') . '">nous contacter</a>.');
+
+define('TEXT_MAIN_LOGOFF', 'Vous avez fermé votre session. Vous pouvez laisser votre ordinateur allumé sans risque d\'utilisation de votre compte.<br /><br />Votre panier en cours a été sauvegardé, ses produits vous seront proposés lors de votre prochaine ouverture de session.');
+
+define('TEXT_MAIN_PASSWORD_FORGOTTEN', 'Si vous avez oublié votre mot de passe, entrez votre adresse électronique ci-dessous et nous vous enverrons un courrier électronique contenant les instructions pour changer votre nouveau mot de passe en toute sécurité.');
+
+define('TEXT_PASSWORD_RESET_INITIATED','Veuillez controller vos emails afin de prendre connaissance des instructions pour modifier votre mot de passe. Les instructions contiennent un lienqui sera valide durant 24 heures ou jusqu\'à la modification de votre mot de passe.');
+
+define('TEXT_NO_EMAIL_ADDRESS_FOUND', 'Erreur: L\'adresse email n\'a pas été trouvé dans notre base de données. Merci d\'essayer à nouveau.');
+
+define('EMAIL_PASSWORD_RESET_SUBJECT', STORE_NAME . ' - Nouveau mot de passe');
+define('EMAIL_PASSWORD_RESET_BODY', 'Un nouveau mot de passe a été demandé pour votre compte client chez ' . STORE_NAME . '.' . "\n\n" . 'Merci de cliquez sur le lien suivant afin de modifier votre mot de passe en toute sécurité:' . "\n\n" . '%s' . "\n\n" . 'Ce lien sera automatiquement invalide après 24h si vous n\'avez pas modifié votre mot de passe' . "\n\n" . 'Pour toute aide complémentaire, veuillez contacter le propriétaire de la boutique à l\'adresse suivante :' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n");
+
+define('ERROR_ACTION_RECORDER' ,'Erreur: Une demande de modification de mot de passe contenant les instructions pour le faire vous a déjà été envoyée par email. Merci de renouveler votre demande dans %s minutes.');
+
+define('TEXT_MAIN_PASSWORD_RESET', 'Veuillez entrer un nouveau mot de passe pour votre compte');
+
+define('TEXT_NO_RESET_LINK_FOUND', 'Erreur: Le lien de mise à jour de votre mot de passe n\'a pas été trouvé dans nos enregistrements. Veuillez essayer à nouveau de générer un nouveau lien.');
+
+define('SUCCESS_PASSWORD_RESET', 'Votre mot de passe a été modifié avec succès. Veuillez vous identifier avec votre nouveau mot de passe.');
+
+/* twig */
+define('FORM_HELPER', 'Survoler pour afficher l\'aide');
+define('CREATE_ACCOUNT_INFO', 'Info');
+define('CREATE_ACCOUNT_FORM_ERROR_TITLE','Erreur dans votre formulaire');
+define('CREATE_ACCOUNT_FORM_ERROR','Vous avez ');
+define('CREATE_ACCOUNT_FORM_ERROR_NUMBERS',' erreur(s) dans votre formulaire.<br />Merci d\'appliquer les corrections nécessaires');
+define('CREATE_ACCOUNT_FIELD_REQUIRED','Ce champs est obligatoire !');
+?>
