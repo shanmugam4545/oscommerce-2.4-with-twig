6106887ff2320504af4538febfc796ae439370cb
Laurent <Foxp2projects@gmail.com>
2013-05-12 13:21:49 +0200

    added : language : french : modules : shipping

---
+++ includes/languages/french/modules/shipping/zones.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_ZONES_TEXT_TITLE', 'Tableau de coût de livraison par zone et régions');
+define('MODULE_SHIPPING_ZONES_TEXT_DESCRIPTION', 'Coût d\'envoie par zone et région');
+define('MODULE_SHIPPING_ZONES_TEXT_WAY', 'Livraison vers');
+define('MODULE_SHIPPING_ZONES_TEXT_UNITS', 'Kg');
+define('MODULE_SHIPPING_ZONES_INVALID_ZONE', 'Pas de livraison possible vers le pays sélectionné');
+define('MODULE_SHIPPING_ZONES_UNDEFINED_RATE', 'Le cout de livraison ne peut etre déterminé pour le moment');
+?>
