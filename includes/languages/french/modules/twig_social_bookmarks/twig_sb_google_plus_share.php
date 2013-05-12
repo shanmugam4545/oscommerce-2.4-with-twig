6eebdbe2a13bfea881ebdef3b313a70dad2b4602
Laurent <Foxp2projects@gmail.com>
2013-05-12 10:04:49 +0200

    added : language : french : module social bookmarks for Twig

---
+++ includes/languages/french/modules/twig_social_bookmarks/twig_sb_google_plus_share.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2012 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('TWIG_MODULE_SOCIAL_BOOKMARKS_GOOGLE_PLUS_SHARE_TITLE', 'Google+ Share');
+  define('TWIG_MODULE_SOCIAL_BOOKMARKS_GOOGLE_PLUS_SHARE_DESCRIPTION', 'Partager les produits sur Google+');
+  define('TWIG_MODULE_SOCIAL_BOOKMARKS_GOOGLE_PLUS_SHARE_PUBLIC_TITLE', 'Partager sur Google+');
+?>
---
+++ includes/languages/french/modules/twig_social_bookmarks/twig_sb_pinterest.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2012 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('TWIG_MODULE_SOCIAL_BOOKMARKS_PINTEREST_TITLE', 'Pinterest');
+  define('TWIG_MODULE_SOCIAL_BOOKMARKS_PINTEREST_DESCRIPTION', 'Partager sur Pinterest');
+  define('TWIG_MODULE_SOCIAL_BOOKMARKS_PINTEREST_PUBLIC_TITLE', 'Epingler ça');
+?>
