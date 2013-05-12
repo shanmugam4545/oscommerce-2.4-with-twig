07c2ab0b69dd3af8cd0d8c0d4fb35eba65cb5915
Laurent <Foxp2projects@gmail.com>
2013-05-12 11:55:56 +0200

    added : language : french : file : products

---
+++ includes/languages/french/products.php
@@ -0,0 +1,74 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+define('NAVBAR_TITLE_SPECIALS', 'Promotions');
+define('NAVBAR_TITLE_NEW', 'Nouveaux produits');
+define('NAVBAR_TITLE_REVIEWS', 'Avis des clients');
+
+define('HEADING_TITLE_SPECIALS', 'Profitez-en tant qu\'ils sont à ce prix !');
+define('HEADING_TITLE_NEW', 'Nouveaux produits');
+define('HEADING_TITLE_REVIEWS', 'Lisez ce que les autres pensent');
+
+define('TEXT_PRODUCT_NOT_FOUND', 'Produit non trouvé !');
+define('TEXT_CURRENT_REVIEWS', 'Avis des clients sur le produit :');
+define('TEXT_MORE_INFORMATION', 'Pour plus d\'informations, veuillez visiter cette <a href="%s" target="_blank"><u>page web</u></a>.');
+define('TEXT_DATE_ADDED', 'Ce produit a été ajouté à notre catalogue le %s.');
+define('TEXT_DATE_AVAILABLE', '<font color="#ff0000">Ce produit sera en stock le %s.</font>');
+define('TEXT_ALSO_PURCHASED_PRODUCTS', 'Les clients qui ont acheté ce produit ont aussi acheté ');
+define('TEXT_PRODUCT_OPTIONS', 'Options :');
+define('TEXT_CLICK_TO_ENLARGE', 'Cliquer pour agrandir');
+
+define('TEXT_DATE_ADDED_NEW', 'Ajouté le :');
+define('TEXT_MANUFACTURER_NEW', 'Fabricant :');
+define('TEXT_PRICE_NEW', 'Prix :');
+
+define('NAVBAR_TITLE_TELL_A_FRIEND', 'Faire connaître à un contact');
+define('HEADING_TITLE_TELL_A_FRIEND', 'Faire connaître à un contact le produit \'%s\'');
+
+define('FORM_TITLE_CUSTOMER_DETAILS', 'Vous');
+define('FORM_TITLE_FRIEND_DETAILS', 'Votre contact');
+define('FORM_TITLE_FRIEND_MESSAGE', 'Votre message');
+
+define('FORM_FIELD_CUSTOMER_NAME', 'Votre nom :');
+define('FORM_FIELD_CUSTOMER_EMAIL', 'Votre adresse électronique :');
+define('FORM_FIELD_FRIEND_NAME', 'Le nom de votre contact :');
+define('FORM_FIELD_FRIEND_EMAIL', 'L\adresse électronqiue de votre ami(e) :');
+
+define('TEXT_EMAIL_SUCCESSFUL_SENT', 'Votre courrier électronique à propos de <strong>%s</strong> a été envoyé à <strong>%s</strong>.');
+
+define('TEXT_EMAIL_SUBJECT', 'Votre ami %s vous recommande la visite du site %s');
+define('TEXT_EMAIL_INTRO', 'Hé %s !' . "\n\n" . 'Votre ami(e), %s, a pensé que vous seriez intéressés par %s de %s.');
+define('TEXT_EMAIL_LINK', 'Pour voir le produit cliquez sur le lien ci-dessous ou copiez et collez le lien dans votre navigateur Internet :' . "\n\n" . '%s');
+define('TEXT_EMAIL_SIGNATURE', 'Cordialement,' . "\n\n" . '%s');
+
+define('ERROR_TO_NAME', 'Erreur : Le nom de votre contact ne doit pas être vide.');
+define('ERROR_TO_ADDRESS', 'Erreur : L\'adresse électronique de votre contact doit être une adresse électronique valide.');
+define('ERROR_FROM_NAME', 'Erreur : Votre nom ne doit pas être vide.');
+define('ERROR_FROM_ADDRESS', 'Erreur : Votre adresse électronique doit être une adresse électronique valide.');
+define('ERROR_ACTION_RECORDER', 'Erreur: Un e-mail a déjà été envoyé. Veuillez ré-essayer dans %s minutes.');
+
+define('TEXT_OF_5_STARS', '%s sur 5 étoiles !');
+
+define('SUB_TITLE_PRODUCT', 'Produit :');
+define('SUB_TITLE_FROM', 'De :');
+define('SUB_TITLE_DATE', 'Date :');
+define('SUB_TITLE_REVIEW', 'Avis :');
+define('SUB_TITLE_RATING', 'Classement :');
+
+define('TEXT_NO_HTML', '<small><font color="#ff0000"><strong>REMARQUE:</strong></font></small>&nbsp;Le code HTML n\'est pas pris en compte !');
+define('TEXT_BAD', '<small><font color="#ff0000"><strong>MAUVAIS</strong></font></small>');
+define('TEXT_GOOD', '<small><font color="#ff0000"><strong>BON</strong></font></small>');
+
+define('TEXT_REVIEW_RECEIVED', 'Merci d\'avoir donné votre avis. Il a été soumis à l\'administrateur du site et sera publié prochainement.');
+
+/* twig */
+define('TEXT_DATE_AVAILABLE_TWIG', 'Ce produit sera disponible le ');
+?>
