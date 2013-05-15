67924b4bf812e67130dfd41404a7a947375e3810
Laurent <Foxp2projects@gmail.com>
2013-05-15 06:51:43 +0200

    modified : manufacturer info boxe -> PDO Ready

--- includes/modules/boxes/model/twig_bm_manufacturer_info.php
+++ includes/modules/boxes/model/twig_bm_manufacturer_info.php
@@ -13,25 +13,27 @@ require(DIR_WS_MODULES . 'boxes/bm_manufacturer_info.php');
     var $code = 'twig_bm_manufacturer_info';
     
     public function execute() { 
-      global $OSCOM_APP;
+      global $OSCOM_APP, $OSCOM_PDO;
       
-      $manufacturer_info = array();
+      $manufacturer_info_box = array();
         
       if ( ($OSCOM_APP->getCode() == 'products') && is_null($OSCOM_APP->getCurrentAction()) && isset($_GET['id']) && !empty($_GET['id']) ) {
-        $manufacturer_query = osc_db_query("select m.manufacturers_id, m.manufacturers_name, m.manufacturers_image, mi.manufacturers_url from " . TABLE_MANUFACTURERS . " m left join " . TABLE_MANUFACTURERS_INFO . " mi on (m.manufacturers_id = mi.manufacturers_id and mi.languages_id = '" . (int)$_SESSION['languages_id'] . "'), " . TABLE_PRODUCTS . " p  where p.products_id = '" . osc_get_prid($_GET['id']) . "' and p.manufacturers_id = m.manufacturers_id");
-        if (osc_db_num_rows($manufacturer_query)) {
-          $manufacturer = osc_db_fetch_array($manufacturer_query);
+        $Qmanufacturerbox = $OSCOM_PDO->prepare('select m.manufacturers_id, m.manufacturers_name, m.manufacturers_image, mi.manufacturers_url from :table_manufacturers m left join :table_manufacturers_info mi on (m.manufacturers_id = mi.manufacturers_id and mi.languages_id = :language_id) , :table_products p where p.products_id = :pid and p.manufacturers_id = m.manufacturers_id');
+        $Qmanufacturerbox->bindInt(':language_id', $_SESSION['languages_id']);
+        $Qmanufacturerbox->bindInt(':pid', osc_get_prid($_GET['id']));
+        $Qmanufacturerbox->execute();
+        
+        if ($Qmanufacturerbox !== false) {
           
-          $manufacturer_info = $manufacturer;
-        }
-      
+          $manufacturer_info_box = $Qmanufacturerbox->fetchall();
+        }      
 
-      $data = array('data' => $manufacturer_info,
-          'group' => $this->group,
-          'boxe' => $this->code,
-          'enabled' => $this->enabled,
-          'sort_order' => $this->sort_order,
-          'title' => $this->title);
+      $data = array('data' => $manufacturer_info_box,
+                    'group' => $this->group,
+                    'boxe' => $this->code,
+                    'enabled' => $this->enabled,
+                    'sort_order' => $this->sort_order,
+                    'title' => $this->title);
 
       return $data;
       }
