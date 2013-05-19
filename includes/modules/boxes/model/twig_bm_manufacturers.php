da597768e580f07f48f9a7d4ec41fe6b510685a5
Laurent <Foxp2projects@gmail.com>
2013-05-19 07:53:33 +0200

    modified : manufacturers boxe

--- includes/modules/boxes/model/twig_bm_manufacturers.php
+++ includes/modules/boxes/model/twig_bm_manufacturers.php
@@ -12,36 +12,45 @@ require(DIR_WS_MODULES . 'boxes/bm_manufacturers.php');
 class twig_bm_manufacturers extends bm_manufacturers {
 
     var $code = 'twig_bm_manufacturers';
-    
+
     public function getData() {
-        global $OSCOM_PDO;
+        global $OSCOM_PDO, $OSCOM_TwigTemplate;
 
         $manufacturers_box_array = array();
 
+        $Qmanufacturers = $OSCOM_PDO->prepare('select m.manufacturers_id as id, m.manufacturers_name as text, m.manufacturers_image as image, mi.manufacturers_short_description as short_description from :table_manufacturers m, :table_manufacturers_info mi where  m.manufacturers_id = mi.manufacturers_id and mi.languages_id = :language_id order by m.manufacturers_name');
+        $Qmanufacturers->bindInt(':language_id', $_SESSION['languages_id']);
+        $Qmanufacturers->execute();
 
-            $Qmanufacturers = $OSCOM_PDO->prepare('select m.manufacturers_id as id, m.manufacturers_name as text, m.manufacturers_image as image, mi.manufacturers_short_description as short_description from :table_manufacturers m, :table_manufacturers_info mi where  m.manufacturers_id = mi.manufacturers_id and mi.languages_id = :language_id order by m.manufacturers_name');
-            $Qmanufacturers->bindInt(':language_id', $_SESSION['languages_id']);
-            $Qmanufacturers->execute();
+        if ($Qmanufacturers->fetch() !== false) {
 
-            if ($Qmanufacturers->fetch() !== false) {
+            $manufacturers_box_array = $Qmanufacturers->fetchall();
+        }
 
-                $manufacturers_box_array = $Qmanufacturers->fetchall();
+        if (isset($_GET['manufacturers_id'])) {
+            $Qmanufacturers_name = $OSCOM_PDO->prepare('select manufacturers_id as id, manufacturers_name as manufacturer_name from :table_manufacturers where manufacturers_id = :mid');
+            $Qmanufacturers_name->bindInt(':mid', $_GET['manufacturers_id']);
+            $Qmanufacturers_name->execute();
+            if ($Qmanufacturers_name->fetch() !== false) {
+                $OSCOM_TwigTemplate->getTwig()->addGlobal('manufacturer_name', $Qmanufacturers_name->value('manufacturer_name'));
             }
-         return $manufacturers_box_array;
-        
+        }
+        $OSCOM_TwigTemplate->getTwig()->addGlobal('manufacturer_id', isset($_GET['manufacturers_id']) ? $_GET['manufacturers_id'] : '0');
+
+        return $manufacturers_box_array;
     }
 
     public function execute() {
 
-            $data = array('data' => $this->getData(),
-                          'manufacturer_id' => (isset($_GET['manufacturers_id']) ? $_GET['manufacturers_id'] : null),
-                          'group' => $this->group,
-                          'boxe' => $this->code,
-                          'enabled' => $this->enabled,
-                          'sort_order' => $this->sort_order,
-                          'title' => $this->title);
+        $data = array('data' => $this->getData(),
+            'group' => $this->group,
+            'boxe' => $this->code,
+            'enabled' => $this->enabled,
+            'sort_order' => $this->sort_order,
+            'title' => $this->title);
 
-            return $data;
-        }
+        return $data;
     }
+
+}
 ?>
