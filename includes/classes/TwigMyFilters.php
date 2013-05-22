8068dfe0188343d69b0697f3638912ce2aa1202c
Laurent <Foxp2projects@gmail.com>
2013-05-22 09:12:18 +0200

    added : twig extension : filter : breakstring

--- includes/classes/TwigMyFilters.php
+++ includes/classes/TwigMyFilters.php
@@ -15,7 +15,8 @@ class TwigMyFilters extends Twig_Extension
     public function getFilters() 
     {
         return array(  
-            new Twig_SimpleFilter('display_price', 'twig_display_price'),            
+            new Twig_SimpleFilter('display_price', 'twig_display_price'), 
+            new Twig_SimpleFilter('breakstring', 'twig_break_string'), 
             new Twig_SimpleFilter('is_array', '_is_array'),  
             new Twig_SimpleFilter('round_rating', 'round_rating'),  
                      
@@ -35,6 +36,27 @@ function twig_display_price($price,$tax_id,$quantity = 1)
     global $currencies;
     return $currencies->display_price($price,$tax_id,$quantity);
 }
+function twig_break_string($string, $len, $break_char = '-') 
+{
+    $l = 0;
+    $output = '';
+    for ($i=0, $n=strlen($string); $i<$n; $i++) {
+      $char = substr($string, $i, 1);
+      if ($char != ' ') {
+        $l++;
+      } else {
+        $l = 0;
+      }
+      if ($l > $len) {
+        $l = 1;
+        $output .= $break_char;
+      }
+      $output .= $char;
+    }
+
+    return $output;
+}
+
 function round_rating($rating) {
     return round($rating * 2 ) / 2;
 }
